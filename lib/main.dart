74e8356043ca7e9fd9da6903e22242281e29c87b
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2024-01-28 23:12:09 +0545

    firebase configuration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/screen/homescreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
