b86e54cb538fd2cbf8b06b83f7d64ffd8738e569
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2024-01-29 00:05:56 +0545

    added documentation to todo model

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -11,10 +11,12 @@ class TodoModel {
     this.isCompleted = false,
   });
 
+  /// Change the value of [isCompleted] to the opposite of what it is.
   void toggleCompleted() {
     isCompleted = !isCompleted;
   }
 
+  /// [Fetch] all tasks from the database and return a [List] of [TodoModel].
   static Future<List<TodoModel>> fetchTasks() async {
     List<TodoModel> tasks = [];
     QuerySnapshot<Map<String, dynamic>> querySnapshot =
@@ -26,6 +28,7 @@ class TodoModel {
     return tasks;
   }
 
+  /// [Add] a new task to the database.
   static void addTask(String task) async {
     String taskId = FirebaseFirestore.instance.collection('tasks').doc().id;
 
@@ -38,6 +41,7 @@ class TodoModel {
     );
   }
 
+  /// [Update] a task in the database.
   static void updateTask(TodoModel todo) async {
     await FirebaseFirestore.instance
         .collection('tasks')
@@ -45,16 +49,18 @@ class TodoModel {
         .update(todo.toJson());
   }
 
+  /// [Delete] a task from the database.
   static void deleteTask(TodoModel todo) async {
     await FirebaseFirestore.instance.collection('tasks').doc(todo.id).delete();
   }
 
-  // from json
+  /// [Create] a new [TodoModel] from a [Map] of data.
   TodoModel.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         title = json['title'],
         isCompleted = json['isCompleted'];
-// to json
+
+  /// [Convert] a [TodoModel] to a [Map] of data.
   Map<String, dynamic> toJson() => {
         'id': id,
         'title': title,
