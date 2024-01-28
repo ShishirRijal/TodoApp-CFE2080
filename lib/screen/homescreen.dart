a524969a12639b0fa3ef79690a99859d86cba07d
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2024-01-28 23:52:32 +0545

    refactored ui

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -27,35 +27,7 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(
-                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 30.0,
-                  child: Icon(
-                    Icons.list,
-                    size: 30.0,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  'Todo App',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          const _Header(),
           Expanded(
             child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
@@ -91,6 +63,45 @@ class HomeScreen extends StatelessWidget {
                         itemCount: snapshot.data!.length,
                       );
                     })),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+          top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 30.0,
+            child: Icon(
+              Icons.list,
+              size: 30.0,
+              color: Colors.deepPurpleAccent,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'Todo App',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ],
       ),
